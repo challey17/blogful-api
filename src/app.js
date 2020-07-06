746b9578636d5a0ffcbb9c476a34712066f17b46
Chayce Knaub <chknaub@yahoo.com>
2020-07-06 09:38:35 -0400

    post and delete added

--- src/app.js
+++ src/app.js
@@ -4,48 +4,17 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const ArticlesService = require("./articles-service");
+const articlesRouter = require("./articles/articles-router");
 
 const app = express();
-const jsonParser = express.json();
+
 //set up env variables for local and heroku
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get("/articles", (req, res, next) => {
-  const knexInstance = req.app.get("db");
-  ArticlesService.getAllArticles(knexInstance)
-    .then((articles) => {
-      res.json(articles);
-    })
-    .catch(next);
-});
-
-app.post("/articles", jsonParser, (req, res, next) => {
-  const { title, content, style } = req.body;
-  const newArticle = { title, content, style };
-  ArticlesService.insertArticle(req.app.get("db"), newArticle)
-    .then((article) => {
-      res.status(201).location(`/articles/${article.id}`).json(article);
-    })
-    .catch(next);
-});
-
-app.get("/articles/:article_id", (req, res, next) => {
-  const knexInstance = req.app.get("db");
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then((article) => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: `Article doesn't exist` },
-        });
-      }
-      res.json(article);
-    })
-    .catch(next);
-});
+app.use("/articles", articlesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,10 +1,11 @@
-const { expect } = require("chai");
 const knex = require("knex");
 const app = require("../src/app");
-const { makeArticlesArray } = require("./articles.fixtures");
-const supertest = require("supertest");
+const {
+  makeArticlesArray,
+  makeMaliciousArticle,
+} = require("./articles.fixtures");
 
-describe.only("Articles Endpoints", function () {
+describe("Articles Endpoints", function () {
   let db;
 
   before("make knex instance", () => {
@@ -12,7 +13,6 @@ describe.only("Articles Endpoints", function () {
       client: "pg",
       connection: process.env.TEST_DB_URL,
     });
-    //sets database to testDB for tests in testfile
     app.set("db", db);
   });
 
@@ -36,8 +36,26 @@ describe.only("Articles Endpoints", function () {
         return db.into("blogful_articles").insert(testArticles);
       });
 
-      it("GET /articles responds with 200 and all of the articles", () => {
+      it("responds with 200 and all of the articles", () => {
         return supertest(app).get("/articles").expect(200, testArticles);
+      });
+    });
+
+    context(`Given an XSS attack article`, () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle();
+
+      beforeEach("insert malicious article", () => {
+        return db.into("blogful_articles").insert([maliciousArticle]);
+      });
+
+      it("removes XSS attack content", () => {
+        return supertest(app)
+          .get(`/articles`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body[0].title).to.eql(expectedArticle.title);
+            expect(res.body[0].content).to.eql(expectedArticle.content);
+          });
       });
     });
   });
@@ -59,7 +77,7 @@ describe.only("Articles Endpoints", function () {
         return db.into("blogful_articles").insert(testArticles);
       });
 
-      it("GET /articles/:article_id responds with 200 and the specified article", () => {
+      it("responds with 200 and the specified article", () => {
         const articleId = 2;
         const expectedArticle = testArticles[articleId - 1];
         return supertest(app)
@@ -67,9 +85,27 @@ describe.only("Articles Endpoints", function () {
           .expect(200, expectedArticle);
       });
     });
+
+    context(`Given an XSS attack article`, () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle();
+
+      beforeEach("insert malicious article", () => {
+        return db.into("blogful_articles").insert([maliciousArticle]);
+      });
+
+      it("removes XSS attack content", () => {
+        return supertest(app)
+          .get(`/articles/${maliciousArticle.id}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.title).to.eql(expectedArticle.title);
+            expect(res.body.content).to.eql(expectedArticle.content);
+          });
+      });
+    });
   });
 
-  describe.only(`POST /articles`, () => {
+  describe(`POST /articles`, () => {
     it(`creates an article, responding with 201 and the new article`, function () {
       this.retries(3);
       const newArticle = {
@@ -91,11 +127,74 @@ describe.only("Articles Endpoints", function () {
           const actual = new Date(res.body.date_published).toLocaleString();
           expect(actual).to.eql(expected);
         })
-        .then((postRes) =>
-          supertest(app)
-            .get(`/articles/${postRes.body.id}`)
-            .expect(postRes.body)
+        .then((res) =>
+          supertest(app).get(`/articles/${res.body.id}`).expect(res.body)
         );
+    });
+
+    const requiredFields = ["title", "style", "content"];
+
+    requiredFields.forEach((field) => {
+      const newArticle = {
+        title: "Test new article",
+        style: "Listicle",
+        content: "Test new article content...",
+      };
+
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newArticle[field];
+
+        return supertest(app)
+          .post("/articles")
+          .send(newArticle)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` },
+          });
+      });
+    });
+
+    it("removes XSS attack content from response", () => {
+      const { maliciousArticle, expectedArticle } = makeMaliciousArticle();
+      return supertest(app)
+        .post(`/articles`)
+        .send(maliciousArticle)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.title).to.eql(expectedArticle.title);
+          expect(res.body.content).to.eql(expectedArticle.content);
+        });
+    });
+  });
+
+  describe(`DELETE /articles/:article_id`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const articleId = 123456;
+        return supertest(app)
+          .delete(`/articles/${articleId}`)
+          .expect(404, { error: { message: `Article doesn't exist` } });
+      });
+    });
+
+    context("Given there are articles in the database", () => {
+      const testArticles = makeArticlesArray();
+
+      beforeEach("insert articles", () => {
+        return db.into("blogful_articles").insert(testArticles);
+      });
+
+      it("responds with 204 and removes the article", () => {
+        const idToRemove = 2;
+        const expectedArticles = testArticles.filter(
+          (article) => article.id !== idToRemove
+        );
+        return supertest(app)
+          .delete(`/articles/${idToRemove}`)
+          .expect(204)
+          .then((res) =>
+            supertest(app).get(`/articles`).expect(expectedArticles)
+          );
+      });
     });
   });
 });
